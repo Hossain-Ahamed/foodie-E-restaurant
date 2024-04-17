16e2284636f5481648be8690e067d0c073f74c34
Hossain Ahamed <hossainahamed6872@gmail.com>
2024-04-17 16:05:28 +0600

    upate

--- src/pages/Others/QrScanner/QrScanner.jsx
+++ src/pages/Others/QrScanner/QrScanner.jsx
@@ -75,19 +75,14 @@ const QRScanner = () => {
     <div className="w-full h-full mt-5">
         <SetTitle  title="Qr Scan"/>
      
-      <div className="w-full max-w-lg mx-auto h-full max-h-[70vh] flex flex-col justify-center items-start">
-      <div id="cam-qr-result" className=' my-2 mx-auto'>
-            {scannedData && (
-              <Link to={scannedData} className=' font-semibold cursor-pointer shadow px-5 py-2 bg-gray-100  text-blue-600 dark:text-blue-500 hover:underline'>Go to Restaurant Page</Link>
-            ) }
-            
-          </div>
+      <div className="w-full max-w-xl mx-auto h-full max-h-[70vh] flex flex-col justify-center items-start">
+     
         {/* <h1>Scan from WebCam:</h1> */}
         {error && <p className='text-danger-400 mt-10 text-center font-semibold text-xl'>{error}</p>}
         <div>
           <video
             id="qr-video"
-            className="w-full md:max-w-[600px] max-h-[70vh] mt-3"
+            className="w-full md:max-w-[700px] max-h-[70vh] mt-3"
           ></video>
         </div>
         
@@ -97,18 +92,12 @@ const QRScanner = () => {
           <p className='text-center mt-4 text-xs text-indigo-500'>Note: To place an order at the restaurant, please scan the QR code provided on your table. After completing the scan, click the button below to navigate to the restaurant page </p>
         </div>
       </div>
-      {/* {cameras.length > 1 && !scannedData && (
-        <div className='w-full mt-5 flex items-center justify-center' >
-
-          <select id="cam-list" value={selectedCamera} onChange={handleCameraChange}>
-            {cameras.map(camera => (
-              <option key={camera.id} value={camera.id}>
-                {camera.label}
-              </option>
-            ))}
-          </select>
-        </div>
-      )} */}
+      <div id="cam-qr-result" className=' my-2 mx-auto'>
+            {scannedData ||(
+              <Link to={scannedData} className=' font-semibold cursor-pointer shadow px-5 py-2 bg-gray-100  text-blue-600 dark:text-blue-500 hover:underline'>Go to Restaurant Page</Link>
+            ) }
+            
+          </div>
     </div>
   );
 };
